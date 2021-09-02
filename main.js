0a75162ded265b37d5eccd08162b9067a1a9a2b0
Samip <batman@192-168-1-102.tpgi.com.au>
2021-09-02 18:03:31 +1000

    Added the dad joke code

---
+++ main.js
@@ -0,0 +1,24 @@
+const jokeButton = document.querySelector(".laugh");
+const container = document.querySelector("#container");
+jokeButton.addEventListener("click", (e) => {
+  jokeLoader();
+});
+
+//fetching url using axios
+async function jokeLoader() {
+  try {
+    const createCard = document.createElement("div");
+    createCard.classList.add("card");
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    const getHeader = {
+      headers: { Accept: "text/plain - Plain text response" },
+    };
+    const getJokes = await axios.get("https://icanhazdadjoke.com/", getHeader);
+    cardBody.append(getJokes.data);
+    createCard.append(cardBody);
+    container.append(createCard);
+  } catch (e) {
+    cardBody.append("Error fetching Joke");
+  }
+}
