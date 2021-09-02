49c8ef68b805c6355ffe2301e3298c86d97b0b29
Samip <batman@192-168-1-102.tpgi.com.au>
2021-09-02 18:19:23 +1000

    Minor changes, no change to the code base

--- main.js
+++ main.js
@@ -7,14 +7,14 @@ jokeButton.addEventListener("click", (e) => {
 //fetching url using axios
 async function jokeLoader() {
   try {
-    const createCard = document.createElement("div");
-    createCard.classList.add("card");
-    const cardBody = document.createElement("div");
-    cardBody.classList.add("card-body");
     const getHeader = {
       headers: { Accept: "text/plain - Plain text response" },
     };
     const getJokes = await axios.get("https://icanhazdadjoke.com/", getHeader);
+    const createCard = document.createElement("div");
+    createCard.classList.add("card");
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
     cardBody.append(getJokes.data);
     createCard.append(cardBody);
     container.append(createCard);
